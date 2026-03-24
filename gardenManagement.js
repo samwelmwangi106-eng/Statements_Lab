2683ea7889d55976f2aee502fc94db68238d0af4
Samwel <samwelmwangi106@gmail.com>
2026-03-24 23:38:15 +0300

    updated gardenManagement.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,24 @@
 const temperature = 90;
+
+if (temperature>=80){
+    console.log("Watering on")
+}
+else  {
+    console.log("Watering off")
+}
 const timeOfDay = "morning";
+
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
 let soilMoisture = 30;
+
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    console.log(soilMoisture);
+}
+
+
